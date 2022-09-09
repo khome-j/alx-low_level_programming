ea95ec335632ac529cccc41d1fe3e844d07995d8
Joseph Okpako <komeokpako@gmail.com>
2022-09-09 04:54:24 +0100

    task 3 and 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+/* Entry Point
+ * Description: A C program that prints with the put function
+ * Return 0
+ *
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+
+}
