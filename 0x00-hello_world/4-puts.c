b07f7ebd6781c9d8e5a6607e7227404a2ac7a29b
Joseph Okpako <komeokpako@gmail.com>
2022-09-09 04:59:26 +0100

    minor fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,15 @@
-/** 
- *Entry Point
- * Description: A C program that prints with the put function
- * Return 0
- *
- */
-
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
