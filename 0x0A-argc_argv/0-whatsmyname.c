dcc92b9d2e5dbc0292ee27566918841f4c8bb0e2
Joseph Okpako <komeokpako@gmail.com>
2022-10-03 10:09:16 +0100

    minor fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 /**
  * main - This program prints its name followed by a new line.
+ *
  * @argc: argument counts
  * @argv: argument vector; which is basically a one dimensional array.
+ *
  * Return: Always 0.
  */
 int main(int __attribute__((unused)) argc, char *argv[])
