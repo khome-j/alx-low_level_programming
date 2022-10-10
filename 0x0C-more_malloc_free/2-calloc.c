aa32aafc90ce04f9db10793aa0ee2ff82a708dc4
Joseph Okpako <komeokpako@gmail.com>
2022-10-10 11:04:48 +0100

    make betty compliant

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,17 +10,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i, *ptr;
+	unsigned int i;
+	char *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc(nmemb * sizeof(*ptr));
+	ptr = malloc(nmemb * size);
 
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		ptr[i] = 0;
 	return (ptr);
 }
-
