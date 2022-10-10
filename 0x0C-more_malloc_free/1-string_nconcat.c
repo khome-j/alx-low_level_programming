b3d0ca6fd856763e27717f31f5a52959d695f0cc
Joseph Okpako <komeokpako@gmail.com>
2022-10-10 08:54:40 +0100

    added task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,6 +17,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int i, j;
 	unsigned int len1, len2;
 
+	/*treat NULL as empty string*/
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		len1++;
 	for (i = 0; s2[i] != '\0'; i++)
