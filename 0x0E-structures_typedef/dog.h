d5a3a2425f6052e964e7e55824b086cbfa034e00
Joseph Okpako <komeokpako@gmail.com>
2022-10-11 15:16:08 +0100

    added header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-
 /**
  * struct dog - struct type for a dog
  * @name: a char pointer
@@ -17,6 +16,11 @@ struct dog
 	float *age;
 	char *owner;
 };
+
+/**
+ * dog_t -Typedef for struct dog
+*/
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
