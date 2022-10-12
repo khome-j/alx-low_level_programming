1a99cc296773fa2fbaaf9ac2945e3153bb175652
Joseph Okpako <komeokpako@gmail.com>
2022-10-12 05:11:13 +0100

    added task 0 to task 2

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,25 +2,17 @@
 #include "dog.h"
 
 /**
- * init_dog - initializes a variable type struct dog
- * @d: input pointer to the struct dog
- * @name: input for dog name
- * @age: input for dog age
- * @owner: input for dog owner
- *
- * Return: nothing
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner to initialize
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *d;
-
-	d = malloc(sizeof(struct dog));
-	if (ptr == NULL)
-		return (NULL);
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
-	return (d);
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
-
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,29 +2,30 @@
 #define DOG_H
 
 /**
- * struct dog - struct type for a dog
- * @name: a char pointer
- * @age: a float pointer
- * @owner: a char pointer
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
  *
- * Description: a struct type showing the name, age and
- * owner of the dog
+ * Description: Longer description
  */
 struct dog
 {
 	char *name;
-	float *age;
+	float age;
 	char *owner;
 };
 
 /**
- * dog_t -Typedef for struct dog
-*/
+ * dog_t - typedef for struct dog
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
-#endif /* struct dog */
+#endif /*DOG_H*/
