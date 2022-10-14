4f6b00ac4eec7017c10dd8c5d21c59e0c48a5501
Joseph Okpako <komeokpako@gmail.com>
2022-10-14 15:14:03 +0100

    make task 1 betty compliant

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,8 +17,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(ap, int));
-		if (separator && i != NULL)
+		printf("%d", va_arg(ap, unsigned int));
+		if (separator && i != n-1 )
 			printf("%s", separator);
 	}
 	va_end(ap);
