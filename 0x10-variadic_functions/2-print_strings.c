7c159f55a85c2923d84afa9c9e9634b4a972caca
Joseph Okpako <komeokpako@gmail.com>
2022-10-15 10:35:15 +0100

    minor fix to header files and task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,11 +14,16 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list ap;
+	char *str;
 
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(ap, char *));
+		str = va_arg(ap, char *);
+		if (str)
+			printf("%s", str);
+		else
+			printf("nil");
 		if (separator && i != n - 1)
 			printf("%s", separator);
 	}
