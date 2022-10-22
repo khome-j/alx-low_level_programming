d37185780af66011022bce41b6066bf032969003
Joseph Okpako <komeokpako@gmail.com>
2022-10-22 04:13:22 +0100

    added readme and task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - a function that prints all the elements of a list.
+ * @h: a pointer of struct type list_t.
+ *
+ * Return: the number of nodes, or nil if str is NULL.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t node = 0;
+
+	while (h != (list_t *) 0)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		printf("[%i] %s\n", h->len, h->str);
+		node++;
+		h = h->next;
+	}
+	return (node);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,6 @@
+0x12. C - Singly linked lists
+For this project, we looked at the following concept:
+Data Structures
+Linked Lists
+Linked list vs Arrays
+How to build and use linked lists
