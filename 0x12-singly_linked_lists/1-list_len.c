7deb945c1ff0f6e499f234390d7d25db69f60632
Joseph Okpako <komeokpako@gmail.com>
2022-10-22 04:39:35 +0100

    made a minor fix on task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,7 @@
+#include <stdio.h>
 #include "lists.h"
-
+#include <stdlib.h>
+#include <string.h>
 /**
  * list_len - a function that returns the number of elements in a linked list
  * @h: a pointer of struct type list_s
