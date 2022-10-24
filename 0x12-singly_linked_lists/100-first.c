6dbf5afdcf4e3dbb069c62abdb9a56e6e6ba646c
Joseph Okpako <komeokpako@gmail.com>
2022-10-24 14:36:39 +0100

    added task5 and modified my header file

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+void before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+};
+
+int main(void)
+{
+    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+    return (0);
+}
