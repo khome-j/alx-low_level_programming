f1d5030598831d8594fadc2167c5037fe4e3247a
Joseph Okpako <komeokpako@gmail.com>
2022-10-25 04:49:03 +0100

    modified header file "lists_h" and added task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_int - a function that prints all the elements
+ * @h: pointer of type listint
+ *
+ * Return: the number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	int node = 0;
+
+	while (h)
+	{
+		node++;
+		h = h->next;
+	}
+	return (node);
+}
