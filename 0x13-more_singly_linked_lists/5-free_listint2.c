dd8fe25338a03c6dad9f528a4bf3be710a937b55
Joseph Okpako <komeokpako@gmail.com>
2022-10-26 04:28:29 +0100

    simultaneously added task 5 and 6

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - a function that frees a list
+ * @head: a pointer of type listint_t
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != (listint_t *) 0)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
+}
---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - a function that deletes the head node of a list
+ * @head: pointer of type listint
+ *
+ * Return: 0 if the linked list is empty
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int i;
+
+	if (*head == NULL)
+		return (0);
+
+
+	temp = (*head)->next;
+	i = (*head)->n;
+	*head = temp;
+	return (i);
+}
