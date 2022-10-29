1f100fc5359d215db413fd4f270d3c9cb8ba43ab
Joseph Okpako <komeokpako@gmail.com>
2022-10-29 15:11:58 +0100

    added task one, readme and headerfile

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,6 +5,10 @@
 #include <string.h>
 #include <stdlib.h>
 
+int _pow(int a, int b);
+int _strlen(const char* b);
+int _atoi(char b);
+
 unsigned int binary_to_unint(const char *b);
 
 #endif /*MAIN_H*/
