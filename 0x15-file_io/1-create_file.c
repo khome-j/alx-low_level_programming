283da91f6e5e36d6a0c0ce490c13a85542a7d07f
Joseph Okpako <komeokpako@gmail.com>
2022-11-03 14:51:33 +0100

    added task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 
 	bytes_read = len;
 	write_file = write(fd, text_content, bytes_read);
-	if (write_file == -1)
+	if (write_file == -1 || text_content == NULL)
 		return (-1);
 	close(fd);
 	return (1);
