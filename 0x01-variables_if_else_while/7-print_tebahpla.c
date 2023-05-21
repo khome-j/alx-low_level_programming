c8ac2d94a62bea522c9900bf0eeddf5e55361b6a
Joseph Okpako <komeokpako@gmail.com>
2023-05-21 13:52:06 +0100

    Task 7 added

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints the lowercase alphabet in reverse.
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; z--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
