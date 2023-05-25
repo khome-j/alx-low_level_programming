9e84e2dd772a56d4d349120d9940c92e30444b2f
Joseph Okpako <komeokpako@gmail.com>
2023-05-25 14:47:24 +0100

    Header file modified

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 #include <unistd.h>
 
 int _putchar(char);
@@ -14,5 +13,6 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
 
 #endif
