75dc705ecd7b58216e4e258d7e67b3edbc39aab8
Joseph Okpako <komeokpako@gmail.com>
2023-05-27 18:11:31 +0100

    header file updated

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,9 +2,12 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <unistd.h>
 
+int _putchar(char);
 int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
+void print_numbers(void);
 
 #endif /* MAIN_H */
