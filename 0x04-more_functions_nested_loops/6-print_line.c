d7e97a9a8abb8db1d3e9af0e83628e43ffce479a
Joseph Okpako <komeokpako@gmail.com>
2023-05-28 10:19:59 +0100

    Task 6 adde

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - a function that draws a straight line in the terminal.
+ * @n: an integer that represents the number of times the character _ should be
+ * printed.
+ *
+ * Return: nothing.
+ */
+
+void print_line(int n)
+{
+	int count;
+	
+	if (!(n <= 0))
+	{
+		count = 0;
+		while (count < n)
+		{
+			_putchar('_');
+			count++;
+		}
+	}
+	_putchar('\n');
+}
