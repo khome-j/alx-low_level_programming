3554069af1135944e72afca0adc7fed0bba01da4
Joseph Okpako <komeokpako@gmail.com>
2023-05-28 09:48:16 +0100

    Task 5 modified

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,9 +10,9 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i < 14; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 10; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j > 9)
 			{
