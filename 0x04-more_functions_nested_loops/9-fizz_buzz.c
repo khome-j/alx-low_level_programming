2041d9f7f6999a60842fc0ed90fdbfb413a90580
Joseph Okpako <komeokpako@gmail.com>
2023-05-29 12:01:02 +0100

    Task 9 added

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: The fizz-buzz test is a program that prints from 1 to 100
+ * followed by a new line. but for the multiples of three print fizz and
+ * multiples of five print buzz. for numbers which are multiples of both 3
+ * and five print fizzbuzz.
+ *
+ * Return: 0 (if successful)
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 1; digit <= 100; digit++)
+	{
+		if (digit % 3 == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+		}
+		else if (digit % 5 == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else if ((digit % 3 == 0) && (digit % 5 == 0))
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
+		else
+		{
+			printf("%d", digit);
+			printf(" ");
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
