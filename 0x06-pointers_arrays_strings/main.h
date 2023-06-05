7120ac4b62e385d485dd2f780eb4117ac553e1ec
Joseph Okpako <komeokpako@gmail.com>
2023-06-05 07:50:35 +0100

    header file added

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@
 char *_strcat(char *, char *);
 char *_strncat(char *, char *, int);
 char *_strncpy(char *, char *, int);
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *, char *);
 
 
 #endif /* MAIN_H */
