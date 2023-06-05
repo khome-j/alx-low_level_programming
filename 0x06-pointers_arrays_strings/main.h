ff010aea72f6f489d7f570b182c43cb03facf7d9
Joseph Okpako <komeokpako@gmail.com>
2023-06-05 10:19:28 +0100

    header file updated

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@ char *_strcat(char *, char *);
 char *_strncat(char *, char *, int);
 char *_strncpy(char *, char *, int);
 int _strcmp(char *, char *);
+void reverse_array(int *, int);
 
 
 #endif /* MAIN_H */
