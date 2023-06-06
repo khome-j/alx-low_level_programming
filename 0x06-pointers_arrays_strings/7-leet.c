4c4d9ccbce72ce54fae68606ff43add4ebc3dc8d
Joseph Okpako <komeokpako@gmail.com>
2023-06-06 08:41:08 +0100

    Task 7 modified

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,22 +9,26 @@
 
 char *leet(char *s)
 {
-	int count;
+	int index, count;
+
+	char lc[] = {'a', 'e', 'o', 't', 'l'};
+	char uc[] = {'A', 'E', 'O', 'T', 'L'};
+	char encode[] = {'4','3', '0', '7', '1'};
 
 	count = 0;
 	while (*(s + count) != '\0')
 	{
-		if (*(s + count) == 'a' || *(s + count) == 'A')
-			*(s + count) = '4';
-		else if (*(s + count) == 'e' || *(s + count) == 'E')
-			*(s + count) = '3';
-		else if (*(s + count) == 'o' || *(s + count) == 'O')
-			*(s + count) = '0';
-		else if (*(s + count) == 't' || *(s + count) == 'T')
-			*(s + count) = '7';
-		else if (*(s + count) == 'l' || *(s + count) == 'L')
-			*(s + count) = '1';
+		for (index = 0; index < 5/*sizeof(lc) / sizeof(char)*/; index++)
+		{
+			if (s[count] == lc[index] || s[count] == uc[index])
+			{
+				s[count] = encode[index];
+				break;
+			}
+
+		}
 		count++;
 	}
+
 	return (s);
 }
