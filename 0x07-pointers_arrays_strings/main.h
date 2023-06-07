2c4457b9fba8bb7fef5707d5d574b74cea83f99e
Joseph Okpako <komeokpako@gmail.com>
2023-06-07 04:28:53 +0100

    header file updated

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,12 +4,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
-
+int _putchar(char);
 char *_memset(char *, char, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
 char *_strchr(char *, char c);
 unsigned int _strspn(char *, char *);
 char *_strpbrk(char *, char *);
 char *_strstr(char *, char *);
+void print_chessboard(char (*a)[8]);
 
 #endif /* MAIN_H */
