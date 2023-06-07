df001f9711457b112e96a8a2f995055a6ec78ead
Joseph Okpako <komeokpako@gmail.com>
2023-06-07 03:32:50 +0100

    header file modified

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,6 +8,6 @@
 char *_memset(char *, char, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
 char *_strchr(char *, char c);
-
+unsigned int _strspn(char *, char *);
 
 #endif /* MAIN_H */
