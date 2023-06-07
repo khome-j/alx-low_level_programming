68dfc983a092a1443da41b52dacf8406dde653ab
Joseph Okpako <komeokpako@gmail.com>
2023-06-07 03:53:58 +0100

    header file updated

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ char *_memset(char *, char, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
 char *_strchr(char *, char c);
 unsigned int _strspn(char *, char *);
+char *_strpbrk(char *, char *);
 
 #endif /* MAIN_H */
