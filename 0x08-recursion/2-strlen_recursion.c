ef68afd297f4295a959f5c023f1fd21a4d612a1e
Joseph Okpako <komeokpako@gmail.com>
2023-06-10 09:48:27 +0100

    Task 2 added

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int _strlen_recursion - a function that returns the length of a string
+ * _strlen_recursion - a function that returns the length of a string
  * @s: a pointer to a string
  *
  * Return: the number of bytes in the string pointed to by s
