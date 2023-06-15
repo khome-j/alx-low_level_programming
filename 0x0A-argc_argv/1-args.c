ed63bcfa70557b6ec4f214da814a8242abd91cc3
Joseph Okpako <komeokpako@gmail.com>
2023-06-15 21:48:32 +0100

    Task one

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, a function that prints the number of arguments passed in
+ * to it.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char* argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
