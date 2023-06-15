f03e653bf8f99e635c4c369fc7c7564e7d93cfa6
Joseph Okpako <komeokpako@gmail.com>
2023-06-15 21:55:11 +0100

    Task one

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,8 +9,8 @@
  * Return: 0 on success
  */
 
-int main(int argc, char* argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
