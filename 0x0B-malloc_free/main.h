d5213074e00a944d4a268c8298d100225087d36a
Joseph Okpako <komeokpako@gmail.com>
2023-06-18 16:02:34 +0100

    header file updated

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -11,7 +11,7 @@ char *create_array(unsigned int, char);
 char *_strdup(char *);
 char *str_concat(char *, char *);
 int **alloc_grid(int, int);
-
+void free_grid(int **, int);
 
 
 #endif /* MAIN_H */
