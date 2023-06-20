5cb0b55bdc4594985cd1911532c893c02709a23a
Joseph Okpako <komeokpako@gmail.com>
2023-06-20 23:30:34 +0100

    headerfile, task 0, and other files added

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * malloc_checked - a program that allocates memory using malloc
+ * @b: size of the memory to be allocated
+ *
+ * Return: a pointer to the allocated memory(address).
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	if (b <= 0)
+		exit(98);
+
+	ptr = malloc(sizeof(void *) * b);
+	if (ptr == NULL)
+		exit(98);
+	return(ptr);
+}
