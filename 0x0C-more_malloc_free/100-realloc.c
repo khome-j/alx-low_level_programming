91673aa2cdec8ccf45e0848d4d39ddf983e5147d
Joseph Okpako <komeokpako@gmail.com>
2023-06-22 04:38:07 +0100

    Task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,11 +17,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr == NULL)
 	{
-		ptr1 = malloc(new_size);
-		if (ptr == NULL)
-			return (NULL);
-		free(ptr);
-		return (ptr1);
+		ptr = malloc(new_size);
+		return (ptr);
 	}
 
 	if (new_size == old_size)
