2a969e2d2a9e831a97625acd02ea93393871ac3c
Joseph Okpako <komeokpako@gmail.com>
2023-06-24 21:31:18 +0100

    Task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,78 +10,12 @@ int str_len(char *name);
  *
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)	
 {
-	struct dog *ptr;
-	char *ptr1, *ptr2;
-	int strlen1 = 0, strlen2 = 0, count;
-
-	strlen1 = str_len(name);
-	strlen2 = str_len(owner);
-
-	ptr = malloc(sizeof(struct dog));
-	if (ptr == NULL)
+	if (d)
 	{
-		free(ptr);
-		exit(1);
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
 	}
-
-	ptr = d;
-	(*ptr).age = age;
-
-	ptr1 = malloc(sizeof(char *) * strlen1);
-	if (ptr1 == NULL)
-	{
-		free(ptr1);
-		exit(1);
-	}
-
-	count = 0;
-	while (name[count] != '\0')
-	{
-		ptr1[count] = name[count];
-		count++;
-	}
-	ptr1[count] = '\0';
-	(*ptr).name = ptr1;
-
-	ptr2 = malloc(sizeof(char *) * strlen2);
-	if (ptr2 == NULL)
-	{
-		free(ptr2);
-		exit(1);
-	}
-
-	count = 0;
-	while (owner[count] != '\0')
-	{
-		ptr2[count] = owner[count];
-		count++;
-	}
-	ptr2[count] = '\0';
-	(*ptr).owner = ptr2;
-
-}
-
-
-
-/**
- * str_len - a program that calculates the lenght of the str
- * @name: a string
- *
- * Return: the lenght of a string on success
- */
-
-int str_len(char *name)
-{
-	int count;
-
-	count = 0;
-
-	while (name[count] != '\0')
-	{
-		count++;
-	}
-
-	return (count);
 }
