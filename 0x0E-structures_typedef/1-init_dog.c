f6d6f224c7a2a5ef102ca29db4f04a215d4c61d0
Joseph Okpako <komeokpako@gmail.com>
2023-06-24 21:32:20 +0100

    Task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,7 +10,7 @@ int str_len(char *name);
  *
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner)	
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
