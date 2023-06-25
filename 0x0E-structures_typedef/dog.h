29f3d19b2b533a62aa21adca9308b5891becf9ca
Joseph Okpako <komeokpako@gmail.com>
2023-06-25 09:35:24 +0100

    header file updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,6 +22,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 typedef struct dog dog_t;
+dog_t *new_dog(char *, float, char *);
 
 #endif /* DOG_H */
 
