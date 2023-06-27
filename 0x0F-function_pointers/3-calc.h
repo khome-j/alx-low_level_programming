9d61f3aafe1c4db2f74d7c5124e133284361f4df
Joseph Okpako <komeokpako@gmail.com>
2023-06-27 10:53:13 +0100

    header file -3

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,29 @@
+#ifndef CALC_H
+#define CALC_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * struct op - struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
+int op_add(int, int);
+int op_sub(int, int);
+int op_mul(int, int);
+int op_div(int, int);
+int op_mod(int, int);
+
+int (*get_op_func(char *))(int, int);
+
+
+#endif
