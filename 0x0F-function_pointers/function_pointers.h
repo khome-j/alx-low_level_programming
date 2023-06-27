5c1663cd1473683d8c711873643c468b0ea24880
Joseph Okpako <komeokpako@gmail.com>
2023-06-27 03:59:13 +0100

    header file updated

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -7,6 +7,7 @@
 
 void print_name(char *, void (*)(char *));
 void array_iterator(int *array, size_t size, void(*action)(int));
+int int_index(int *, int, int (*)(int));
 
 
 #endif /* FUNCTION_H */
