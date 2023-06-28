2f682cfbe0f2387f347fb2f742f7ccd94216844b
Joseph Okpako <komeokpako@gmail.com>
2023-06-28 14:07:05 +0100

    task 1 added

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,7 +12,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 
-	if (separator != NULL && n != 0)
+	if (separator != NULL && (strlen(separator) > 1 && n != 0))
 	{
 		va_list arg;
 
