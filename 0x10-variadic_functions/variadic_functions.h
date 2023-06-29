efa3898730f91ca5de376bafe2276a2fbc13c351
Joseph Okpako <komeokpako@gmail.com>
2023-06-29 08:44:58 +0100

    header file updated

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,5 +9,26 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *, const unsigned int, ...);
 void print_strings(const char *, unsigned int, ...);
+void print_all(const char * const format, ...);
+
+
+/**
+ * struct func_format - a structure that holds the format symbol and a function
+ * to print based on the type of argument(symbol) passed to the function
+ *
+ * @symbol: the symbol that will determine the function to be invoked
+ * @func_ptr: a pointer to the function that will print based on the type of
+ * argument passed
+ */
+typedef struct func_format
+{
+	char *symbol;
+	void (*func_ptr)(va_list);
+} format_l;
+
+void char_print(va_list);
+void int_print(va_list);
+void float_print(va_list);
+void str_print(va_list);
 
 #endif /* FUNCTIONS_H */
