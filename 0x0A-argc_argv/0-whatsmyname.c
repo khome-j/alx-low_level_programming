3ddaf3a8656281322e36ab31368690785ec5b877
Joseph Okpako <komeokpako@gmail.com>
2023-08-05 03:39:58 +0000

    TAsk 0 added

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
