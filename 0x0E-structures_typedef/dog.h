48ce1ca8be6b3cc3d2cfd1cbae67797be995cc37
Joseph Okpako <komeokpako@gmail.com>
2023-08-15 01:01:26 +0000

    READme file, TAsk 0, header_file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - a structure data type for a dog
+ * @name: name of dog which is a string
+ * @age: age of the dog in integer
+ * @owner: owner of the dog in string
+ */
+struct dog{
+	char *name;
+	int age;
+	char *owner;
+};
+
+
+#endif /*DOG_H*/
