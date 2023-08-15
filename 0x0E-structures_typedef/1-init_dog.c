5c6aa714769dbb960fd2dd0edc4158da8cce4610
Joseph Okpako <komeokpako@gmail.com>
2023-08-15 01:36:27 +0000

    quick fix to task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,9 +13,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-		new = d;
-		(*new).name = name;
-		(*new).age = age;
-		(*new).owner = owner;
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
 	}
 }
