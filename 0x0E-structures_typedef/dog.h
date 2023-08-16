b5a502790dbf520eb34a05a9234b5f4368ce8754
Joseph Okpako <komeokpako@gmail.com>
2023-08-16 10:44:05 +0000

    quick fix task 4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,8 +17,8 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog dog_t;
 void init_dog(struct dog *, char *, float, char *);
 void print_dog(struct dog *);
-typedef struct dog dog_t;
-
+dog_t *new_dog(char *, float, char *);
 #endif /*DOG_H*/
