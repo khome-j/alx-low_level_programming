2dc316b0ced03897020ac24b474f9961369f05d2
Joseph Okpako <komeokpako@gmail.com>
2023-08-17 01:52:02 +0000

    TAsk 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,8 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 
-	if (array && (size > 0) && cmp)
-	{
+	if (array && cmp) {
 		i = 0;
 		while (i < size)
 		{
