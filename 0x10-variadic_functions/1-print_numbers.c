a7c391497f2651cbb9c2b1a4548d826a00435329
Joseph Okpako <komeokpako@gmail.com>
2023-08-19 02:35:48 +0000

    minor fix

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,23 +9,18 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	if (separator && n != 0)
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+
+	i = 0;
+	while (i < n)
 	{
-		va_list ap;
-		unsigned int i;
-
-		va_start(ap, n);
-
-		i = 0;
-		while (i < n)
-		{
-			if (i % 10)
-				printf("%s", separator);
-			if (!(i % 10) && i)
-				printf("\n");
-			printf("%d", va_arg(ap, int));
-			i++;
-		}
-		printf("\n");
+		printf("%d", va_arg(ap, int));
+		if (separator && i != n - 1)
+			printf("%s", separator);
+		i++;
 	}
+	printf("\n");
 }
