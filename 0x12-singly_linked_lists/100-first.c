d3d5c2ac7bfcd56f4b184a4df362b4b7169de393
Joseph Okpako <komeokpako@gmail.com>
2023-08-25 03:11:48 +0100

    Task 5 and headerfile updated

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * print - a function that executes before the main function
+ *
+ */
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house	upon my back!\n");
+}
