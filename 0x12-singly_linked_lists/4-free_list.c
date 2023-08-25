c7852005f4c3c5c2988e32ccff57d188735512ce
Joseph Okpako <komeokpako@gmail.com>
2023-08-25 02:55:35 +0100

    task 4, header-file updated

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - a function that frees a list_t list
+ * @head: data type of struct list_s
+ *
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+	
+	while ((temp = head))
+	{
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -24,5 +24,6 @@ size_t print_list(const list_t *);
 size_t list_len(const list_t *);
 list_t *add_node(list_t **, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif /*LISTS_H*/
