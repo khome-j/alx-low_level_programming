c49ed751737a318aee958aaf491e518a5e4ca212
Joseph Okpako <komeokpako@gmail.com>
2023-09-03 21:21:00 +0100

    dereferenced head

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,14 +13,14 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *current, *prev;
 	unsigned int count = 0;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (-1);
-	
-	current = *head; 
+
+	current = *head;
 	prev = *head;
 	if (index == 0) /*we have to delete the first node*/
 	{
-		*head = (*head)->next;
+		*head = current->next;
 		free(current);
 		return (1);
 	}
@@ -36,9 +36,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			}
 			prev = current;
 			current = current->next;
-			count++;
+			++count;
 		}
-		
 	}
 	return (-1);
 }
