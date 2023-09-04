84ee48252bcaeb87ecc6ead5b06bba3a9b50e512
Joseph Okpako <komeokpako@gmail.com>
2023-09-04 06:40:29 +0100

    header file updated

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -33,5 +33,6 @@ listint_t *get_nodeint_at_index(listint_t *, unsigned int);
 int sum_listint(listint_t *);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int, int);
 int delete_nodeint_at_index(listint_t **, unsigned int);
+listint_t *reverse_listint(listint_t **);
 
 #endif /* LISTS_H */
