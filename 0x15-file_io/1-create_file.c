32dc77ff18939c90683d7433402750c31cfd6ba5
Joseph Okpako <komeokpako@gmail.com>
2023-09-06 10:12:17 +0100

    task 1 added

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * _strlen - a function the returns the length of a string
+ * @c: string
+ *
+ * Return: the length of a string on success
+ */
+int _strlen(char *s)
+{
+	int count = 1;
+
+	if (*s == '\0')
+		return (0);
+	return (count + _strlen(++s));
+}
+/**
+ * create_file - a function that creates a file
+ * @filename: name of the file
+ * @text_content: a NULL terminated string
+ *
+ * Return: 1 on success, -1 on failure if file cannot be created, file can not
+ * be written, filename is NULL.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i, write_file;
+	char *buf;
+
+	buf = malloc(sizeof(char) * _strlen(text_content));
+	if (filename == NULL || buf == NULL)
+	{
+		free(buf);
+		return (-1);
+	}
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	i = 0;
+	while (text_content[i])
+	{
+		buf[i] = text_content[i];
+		i++;
+	}
+
+	write_file = write(fd, buf, _strlen(text_content));
+	if (write_file == -1)
+		return (-1);
+	close(fd);
+
+	free(buf);
+
+	return (1);
+}
