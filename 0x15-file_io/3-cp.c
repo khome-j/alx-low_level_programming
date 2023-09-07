2bee54e1b999d4449107c19090d667fdcadf06e5
Joseph Okpako <komeokpako@gmail.com>
2023-09-07 08:36:35 +0100

    Task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,102 @@
+#include "main.h"
+#define BUF 1023
+
+
+/**
+ * read_filefrom - a function that reads the content of a file
+ * @file_from: filename -strings.
+ *
+ * Return: Nothing
+ */
+
+char *read_filefrom(const char *file_from)
+{
+	ssize_t fd, read_file, close_file;
+	char *buffer;
+
+	buffer = malloc(BUF);
+	if (buffer == NULL)
+		exit(EXIT_FAILURE);
+
+	fd = open(file_from, O_RDONLY);
+	if (fd == -1)
+		exit(EXIT_FAILURE);
+
+
+	read_file = read(fd, buffer, BUF);
+	if (read_file == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	close_file = close(fd);
+	if (close_file == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close %li\n", fd);
+		exit(100);
+	}
+	return (buffer);
+}
+
+
+/**
+ * cp - a function that copies a the content of a buffer into another file
+ * @buffer: a buffer to copy strings from.
+ * @file_to: the file which strings would be copied into
+ *
+ * Return: Nothing
+ */
+void cp(char *buffer, char *file_to)
+{
+	ssize_t fd, write_file, close_file;
+
+	fd = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd == -1)
+	{
+		exit(EXIT_FAILURE);
+	}
+
+
+	write_file = write(fd, buffer, BUF);
+	if (write_file == -1)/* || read_file != write_file)*/
+	{
+		dprintf(STDERR_FILENO, "Can't write to %s\n", file_to);
+		exit(99);
+	}
+
+
+	close_file = close(fd);
+	if (close_file == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close %li\n", fd);
+		exit(101);
+	}
+
+}
+
+
+
+
+/**
+ * main - Entry point
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success
+ */
+int main(int ac, char **av)
+{
+	char *read_file;
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	read_file = read_filefrom(av[1]);
+	cp(read_file, av[2]);
+
+	return (0);
+}
