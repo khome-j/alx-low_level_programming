ef2ad2727eb61337ec2063fd2973a0362ef4cbde
Joseph Okpako <komeokpako@gmail.com>
2023-09-08 07:56:03 +0100

    Hot fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,93 +2,75 @@
 #include <limits.h>
 #define BUF 1024
 
+/**
+ * exit_code - a function that displays code errors
+ * @code: exit code num
+ * @fd: file descriptor
+ * @str: file_from or file_to string
+ *
+ * Return: nothing
+ */
+void exit_code(int code, ssize_t fd, char *str)
+{
+	switch (code)
+	{
+		case 97:
+			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+			exit(code);
+		case 98:
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", str);
+			exit(98);
+		case 99:
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", str);
+			exit(99);
+		case 100:
+			dprintf(STDERR_FILENO, "Can't close fd %li\n", fd);
+			exit(100);
+	}
+}
 
 /**
- * read_filefrom - a function that reads the content of a file
- * @file_from: filename -strings.
+ * cp - a function that copies the content of a file to another file
+ * @file_from: file from
+ * @file_to: file_to
  *
- * Return: Nothing
  */
-
-char *read_filefrom(const char *file_from)
+void cp(char *file_from, char *file_to)
 {
-	ssize_t fd, read_file, close_file;
+	ssize_t fd, fd1, rfile_from, wfile_to, close_fd, close_fd1;
 	char *buffer;
 
 	buffer = malloc(sizeof(char) * BUF);
 	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
+		free(buffer);
+		exit_code(99, 0, file_from);
 	}
 	fd = open(file_from, O_RDONLY);
 	if (fd == -1)
+		exit_code(98, fd, file_from);
+	fd1 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd1 == -1)
+		exit_code(99, fd, file_to);
+
+	while ((rfile_from = read(fd, buffer, BUF)) != 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
+		if (rfile_from == -1)
+			exit_code(98, fd, file_from);
+
+		wfile_to = write(fd1, buffer, rfile_from);
+		if (wfile_to == -1)
+			exit_code(99, fd, file_to);
 
 	}
+	close_fd = close(fd);
+	if (close_fd == -1)
+		exit_code(100, fd, NULL);
 
-	read_file = read(fd, buffer, BUF);
-	if (read_file == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-
-	close_file = close(fd);
-	if (close_file == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close %li\n", fd);
-		exit(100);
-	}
-	return (buffer);
+	close_fd1 = close(fd1);
+	if (close_fd1 == -1)
+		exit_code(100, fd1, NULL);
 }
-
-
-/**
- * cp - a function that copies a the content of a buffer into another file
- * @buffer: a buffer to copy strings from.
- * @file_to: the file which strings would be copied into
- *
- * Return: Nothing
- */
-void cp(char **buffer, char *file_to)
-{
-	ssize_t fd, write_file, close_file;
-
-	if (file_to == NULL || *buffer == NULL)
-	{
-		dprintf(STDERR_FILENO, "Can't write to %s\n", file_to);
-		exit(99);
-	}
-
-	fd = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Can't write to %s\n", file_to);
-		exit(99);
-	}
-
-
-	write_file = write(fd, *buffer, BUF);
-	if (write_file == -1)
-	{
-		dprintf(STDERR_FILENO, "Can't write to %s\n", file_to);
-		exit(99);
-	}
-
-
-	close_file = close(fd);
-	if (close_file == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close %li\n", fd);
-		exit(100);
-	}
-
-}
-
-
 /**
  * main - Entry point
  * @ac: argument count
@@ -98,16 +80,13 @@ void cp(char **buffer, char *file_to)
  */
 int main(int ac, char **av)
 {
-	char *read_file;
-
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	read_file = read_filefrom(av[1]);
-	cp(&read_file, av[2]);
+	cp(av[1], av[2]);
 
 	return (0);
 }
